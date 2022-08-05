ca9f0163a2641b1246e2c4f3d4519626bf52aa3e
unknown <petrosbeyene611@gmail.com>
2022-08-05 17:07:13 +0300

    Refactor QuestionsPage.js for better data managing

--- src/pages/QuestionsPage.js
+++ src/pages/QuestionsPage.js
@@ -1,30 +1,50 @@
 import React from "react";
+import { useState, useEffect } from "react";
 import AnswerButton from "../components/AnswerButton";
 
 export default function QuestionsPage(){
-    const [questionData, setQuestionData] = React.useState({})
+    const [questionData, setQuestionData] = useState([])
 
-
-    React.useEffect(() => {
+    useEffect(() => {
         fetch("https://opentdb.com/api.php?amount=10&category=21&type=multiple")
         .then(res => res.json())
-        .then(data => setQuestionData(data))
+        .then(data => setQuestionData(data.results))
     }, [])
 
-    const questions = questionData.results.map(question => {
+
+    const questionAndAnswers = questionData.map(questionObj => {
+        
+        const listOfAnswers = questionObj.incorrect_answers
+        if(!questionObj.incorrect_answers.includes(questionObj.correct_answer)){
+            let randomIndex = Math.floor(Math.random() * 4)
+            listOfAnswers.splice(randomIndex, 0, questionObj.correct_answer)
+        }
+        
         return(
-            <div>
-                  <span>{question.question} (difficulty--{question.difficulty})</span><br/>
-                  <AnswerButton value ={question.correct_answer}/>
-                  <AnswerButton value ={question.incorrect_answers[0]}/>
-                  <AnswerButton value ={question.incorrect_answers[1]}/>
-                  <AnswerButton value ={question.incorrect_answers[2]}/>
-                  <hr/>
+            {
+                question: questionObj.question,
+                difficulty: questionObj.difficulty,
+                correct_answer: questionObj.correct_answer,
+                answers: listOfAnswers,
+            }
+        
+        )
+    })
+
+    const questionsList = questionAndAnswers.map((question) => {
+        const answerList = question.answers.map(answer => <AnswerButton value={answer} />)
+        return(
+           <div>
+                <span>{question.question} (difficulty--{question.difficulty})</span><br/>
+                {answerList}
+                <hr/>
             </div>
         )
     })
 
     return(
-        <div>{questions}</div>
+        <div>
+            {questionsList}
+        </div>
     )
 }
