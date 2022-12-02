2f887976b75ac035a8b42b818aa0bd8492696037
unknown <petrosbeyene611@gmail.com>
2022-12-02 15:47:08 +0300

    Handle state changes in answer buttons

--- src/components/AnswerButton.js
+++ src/components/AnswerButton.js
@@ -1,8 +1,14 @@
 import React from "react";
 
 export default function AnswerButton(props){
+    const styles = {
+        backgroundColor: props.isChosen ? 'Green' : null
+    }
     return(
-        <div>
+        <div style={styles}
+        onClick = {props.chooseAnswer}
+        >
+            <p>{props.isChosen ? 'true': 'false'}</p>
             <button>{props.value}</button>
         </div>
     )
--- src/pages/QuestionsPage.js
+++ src/pages/QuestionsPage.js
@@ -1,38 +1,136 @@
+// import React from "react";
+// import { useState, useEffect } from "react";
+// import { nanoid } from 'nanoid';
+// import AnswerButton from "../components/AnswerButton";
+
+// export default function QuestionsPage(){
+//     const [inputData, setInputData] = useState([])
+//     const [questionAndAnswers, setQuestionAndAnswers] = useState([])
+
+//     useEffect(() => {
+//         fetch("https://opentdb.com/api.php?amount=5&category=21")
+//         .then(res => res.json())
+//         .then(data => setInputData(data.results))
+//     }, [])
+
+//     function getAllQ_and_As(){
+//         const allQuestionAndAnswers = inputData.map(questionObj => {
+//             const listOfAnswers = questionObj.incorrect_answers
+//             if(!questionObj.incorrect_answers.includes(questionObj.correct_answer)){
+//                 let randomIndex = Math.floor(Math.random() * 4)
+//                 listOfAnswers.splice(randomIndex, 0, questionObj.correct_answer)
+//             }
+            
+//             const answers = listOfAnswers.map(answer => {
+//                 return {answerValue: answer, isChosen: false, id: nanoid()}
+//             })
+
+//             return ({
+//                     question: questionObj.question,
+//                     difficulty: questionObj.difficulty,
+//                     correct_answer: questionObj.correct_answer,
+//                     answers: answers
+//                 })
+//         })
+
+//         return allQuestionAndAnswers
+//     }
+
+//     useEffect(() => {
+//         setQuestionAndAnswers(getAllQ_and_As())
+//     }, [])
+
+    // function choose(id){
+    //     setQuestionAndAnswers(prevQuestionData => prevQuestionData.map(questionObject => {
+    //         questionObject.answers.map(answer => {
+    //             return id === answer.id ? {...answer, isHled: !answer.isChosen} : answer
+    //         })
+    //         return questionObject
+    //     })) 
+    // }
+
+//     const questionsList = questionAndAnswers.map(quesAndAns => {
+//         const answerList = quesAndAns.answers.map(answer => <AnswerButton key={answer.id} 
+//                                                                         value={answer.answerValue} 
+//                                                                         isChosen = {answer.isChosen} 
+//                                                                         // choose ={()=> {choose(answer.id)}}
+//                                                                         />)
+//         return(
+//            <div>
+//                 <span>{quesAndAns.question} (difficulty--{quesAndAns.difficulty})</span><br/>
+//                 {answerList}
+//                 <hr/>
+//             </div>
+//         )
+//     })
+
+//     return(
+//         <div>
+//             {questionsList}
+//         </div>
+//     )
+// }
 import React from "react";
 import { useState, useEffect } from "react";
 import AnswerButton from "../components/AnswerButton";
+import { nanoid } from 'nanoid';
 
 export default function QuestionsPage(){
-    const [questionData, setQuestionData] = useState([])
+    const [inputData, setInputData] = useState([])
+    const [allQuestionsData, setAllQuestionsData] = useState([])
 
     useEffect(() => {
-        fetch("https://opentdb.com/api.php?amount=10&category=21&type=multiple")
+        fetch("https://opentdb.com/api.php?amount=5&category=21")
         .then(res => res.json())
-        .then(data => setQuestionData(data.results))
+        .then(data => setInputData(data.results))
     }, [])
 
-
-    const questionAndAnswers = questionData.map(questionObj => {
+    function getAllQuestionandAnswers(){
+        const questionAndAnswers = inputData.map(questionObj => {
         
-        const listOfAnswers = questionObj.incorrect_answers
-        if(!questionObj.incorrect_answers.includes(questionObj.correct_answer)){
-            let randomIndex = Math.floor(Math.random() * 4)
-            listOfAnswers.splice(randomIndex, 0, questionObj.correct_answer)
-        }
-        
-        return(
-            {
-                question: questionObj.question,
-                difficulty: questionObj.difficulty,
-                correct_answer: questionObj.correct_answer,
-                answers: listOfAnswers,
+            const listOfAnswers = questionObj.incorrect_answers
+            if(!questionObj.incorrect_answers.includes(questionObj.correct_answer)){
+                let randomIndex = Math.floor(Math.random() * 4)
+                listOfAnswers.splice(randomIndex, 0, questionObj.correct_answer)
             }
-        
-        )
-    })
+    
+            const answers = listOfAnswers.map(answer => {
+                return {answerVal: answer, isChosen: false, id: nanoid()}
+            })
+            
+            return(
+                {
+                    question: questionObj.question,
+                    difficulty: questionObj.difficulty,
+                    correct_answer: questionObj.correct_answer,
+                    answers: answers,
+                }
+            
+            )
+        })
 
-    const questionsList = questionAndAnswers.map((question) => {
-        const answerList = question.answers.map(answer => <AnswerButton value={answer} />)
+        return questionAndAnswers
+    }
+
+    useEffect(()=>{
+        setAllQuestionsData(getAllQuestionandAnswers())
+    },[inputData])
+
+
+    function choose(id){
+        setAllQuestionsData(oldAllQuestionsData => oldAllQuestionsData.map(questionObject => {
+            const newAnswers = questionObject.answers.map(answer => {
+                return id === answer.id ? {...answer, isChosen: !answer.isChosen} : answer
+            })
+            return {...questionObject, answers: newAnswers}
+        })) 
+    }
+
+    const questionsList = allQuestionsData.map((question) => {
+        const answerList = question.answers.map(answer => <AnswerButton value={answer.answerVal} 
+                                                                        key ={answer.id}
+                                                                        isChosen ={answer.isChosen}
+                                                                        chooseAnswer ={()=> choose(answer.id)}/>)
         return(
            <div>
                 <span>{question.question} (difficulty--{question.difficulty})</span><br/>
